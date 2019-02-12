a0d276dee5fa430dfa9e35104ff8e8c57fc4a889
Riwandy <riwandy10@gmail.com>
2019-02-13 01:08:34 +0700

    export array object robots

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 10,
+    name: 'Donuld trump',
+    username: 'Bret',
+    email: 'donuld.trump@gmail.com'
+  },
+  {
+    id: 20,
+    name: 'Nurhadi',
+    username: 'dildojuga',
+    email: 'nurhadi@tronjol.tv'
+  },
+  {
+    id: 30,
+    name: 'Aldo',
+    username: 'dildo',
+    email: 'aldo@yesenia.net'
+  },
+  {
+    id: 40,
+    name: 'Ronaldowati',
+    username: 'wati',
+    email: 'ronaldo.wati@kory.org'
+  },
+  {
+    id: 50,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 60,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 70,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 80,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 90,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 100,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];

9457ee510348dedf0d843a4df911c386a063f853
Riwandy <riwandy10@gmail.com>
2019-02-13 12:04:27 +0700

    change and add some code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CardList from './CardList';
 import { robots } from './robots';
+import SearchBox from './SearchBox';
 
 const App = () => {
   return (
     <div>
       <h1>Robo Friends</h1>
+      <SearchBox></SearchBox>
       <CardList robots={robots} />
     </div>
   );

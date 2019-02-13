91fd2ac0abc02613121a363d619e2d49f8d21eb6
Riwandy <riwandy10@gmail.com>
2019-02-13 11:54:15 +0700

    change somes code

--- src/CardList.js
+++ src/CardList.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  const cardArray = robots.map((user, i) => {
-    return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
-  })
   return (
     <div>
-      {cardArray}
+      {
+        robots.map((user, i) => {
+          return (
+          <Card
+          key={i}
+          id={robots[i].id} 
+          name={robots[i].name} 
+          email={robots[i].email}
+          />
+          );
+        })
+      }
     </div>
   );
 }

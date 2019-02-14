9eede7ee178ff010c2b6af9e4be8f96dd7cdd41b
Riwandi <44519384+rwndy@users.noreply.github.com>
2019-02-14 21:49:51 +0700

    add build's folder

---
+++ build/precache-manifest.ea669bde8c544d7042ed7cacf8b8cabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cc19ea34d5405b6b66a2",
+    "url": "/static/js/main.cc19ea34.chunk.js"
+  },
+  {
+    "revision": "1854fe50e48cef9c32e5",
+    "url": "/static/js/2.1854fe50.chunk.js"
+  },
+  {
+    "revision": "cc19ea34d5405b6b66a2",
+    "url": "/static/css/main.f4e82b63.chunk.css"
+  },
+  {
+    "revision": "1854fe50e48cef9c32e5",
+    "url": "/static/css/2.131f40f1.chunk.css"
+  },
+  {
+    "revision": "ac98fbe81643784e16b783bafb62c8be",
+    "url": "/index.html"
+  }
+];
